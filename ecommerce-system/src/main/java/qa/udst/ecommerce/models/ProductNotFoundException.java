801e323624d8a08b3861743494ea139fab64eb55
infs4102 <infs4102@10233-19.local>
2025-01-19 10:24:01 +0300

    first commit

--- ecommerce-system/src/main/java/qa/udst/ecommerce/models/ProductNotFoundException.java
+++ ecommerce-system/src/main/java/qa/udst/ecommerce/models/ProductNotFoundException.java
@@ -1,5 +1,7 @@
 package qa.udst.ecommerce.models;
 
 public class ProductNotFoundException extends RuntimeException {
-    
+    public ProductNotFoundException(String message) {
+        super(message);
+        }
 }
